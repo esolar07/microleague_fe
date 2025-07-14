aac99ace5ec2ecccbc0cacdd9a30acf55dce57d7
esolar07 <esolar07@gmail.com>
2025-07-14 16:23:16 -0400

    completed reponsive layout for matchup form

--- microleague_fe/components/MatchUpForm.tsx
+++ microleague_fe/components/MatchUpForm.tsx
@@ -34,83 +34,122 @@ const MatchUpForm = () => {
 
     return (
         <Container>
-            <Form onSubmit={handleSubmit}>
-                <div className="space-y-12">
-                    <div className="border-b border-gray-900/10 pb-12">
-                        <h2 className="text-base/7 font-semibold text-gray-900">Create Your Fantasy Match Up.</h2>
-                        <p className="mt-1 text-sm/6 text-gray-600">
-                            Select a sports team from any era.
-                        </p>
-                        <fieldset className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-                            <div className="sm:col-span-4">
-                                <label htmlFor="sport"
-                                       className="block text-sm/6 font-medium text-gray-900">Sport:</label>
-                                <div className="mt-2">
-                                    <div
-                                        className="flex items-center rounded-md bg-white pl-3 outline-1 -outline-offset-1 outline-gray-300 focus-within:outline-2 focus-within:-outline-offset-2 focus-within:outline-indigo-600">
-
-                                        <input
-                                            type="text"
+            <section className="">
+                <Form onSubmit={handleSubmit}>
+                    <div className="space-y-12 max-w-[800px] mx-auto">
+                        <div className="border-b border-red-900/10 pb-12">
+                            <h2 className="text-base/7 font-semibold text-gray-900 text-center">Create Your Fantasy
+                                Match Up.</h2>
+                            <p className="mt-1 text-sm/6 text-gray-600 text-center">Select a sports team from any
+                                era.</p>
+                            <fieldset className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                                <div className="sm:col-span-3">
+                                    <label htmlFor="sport"
+                                           className="block text-sm/6 font-medium text-gray-900">Sport:</label>
+                                    <div className="mt-2 grid grid-cols-1">
+                                        <select
+                                            className="col-start-1 row-start-1 w-full appearance-none rounded-md bg-white py-1.5 pr-8 pl-3 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
                                             id="sport"
                                             name="sport"
                                             value={formData.sport}
                                             onChange={handleChange}
                                             required
-                                        />
+                                        >
+                                            <option>Football</option>
+                                            <option>Basketball</option>
+                                            <option>Baseball</option>
+                                        </select>
                                     </div>
                                 </div>
-                            </div>
-                        </fieldset>
+                            </fieldset>
 
-                        <fieldset>
-                            <label htmlFor="homeTeamName">Home Team Name:</label>
-                            <input
-                                type="text"
-                                id="homeTeamName"
-                                name="homeTeamName"
-                                value={formData.homeTeamName}
-                                onChange={handleChange}
-                                required
-                            />
-                        </fieldset>
+                            <fieldset className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                                <div className="sm:col-span-3">
+                                    <label htmlFor="homeTeamName" className="block text-sm/6 font-medium text-gray-900">Home
+                                        Year/Era:</label>
+                                    <div className="mt-2">
+                                        <select
+                                            className="block w-full appearance-none rounded-md bg-white py-1.5 pr-8 pl-3 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                                            id="homeTeamName"
+                                            name="homeTeamName"
+                                            value={formData.homeTeamName}
+                                            onChange={handleChange}
+                                            required
+                                        >
+                                            <option>2001</option>
+                                            <option>1991</option>
+                                            <option>1981</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div className="sm:col-span-3">
+                                    <label htmlFor="homeTeamYearEra"
+                                           className="block text-sm/6 font-medium text-gray-900">Home Team Name:</label>
+                                    <div className="mt-2">
+                                        <select
+                                            className="block w-full appearance-none rounded-md bg-white py-1.5 pr-8 pl-3 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                                            id="homeTeamYearEra"
+                                            name="homeTeamYearEra"
+                                            value={formData.homeTeamYearEra}
+                                            onChange={handleChange}
+                                        >
+                                            <option>Dallas Cowboys</option>
+                                            <option>Miami Dolphins</option>
+                                            <option>New York Giants</option>
+                                        </select>
+                                    </div>
+                                </div>
+                            </fieldset>
 
-                        <fieldset>
-                            <label htmlFor="homeTeamYearEra">Home Team Year/Era:</label>
-                            <input
-                                type="text"
-                                id="homeTeamYearEra"
-                                name="homeTeamYearEra"
-                                value={formData.homeTeamYearEra}
-                                onChange={handleChange}
-                            />
-                        </fieldset>
-
-                        <fieldset>
-                            <label htmlFor="visitingTeamName">Visiting Team Name:</label>
-                            <input
-                                type="text"
-                                id="visitingTeamName"
-                                name="visitingTeamName"
-                                value={formData.visitingTeamName}
-                                onChange={handleChange}
-                                required
-                            />
-                        </fieldset>
-
-                        <fieldset>
-                            <label htmlFor="visitingTeamYearEra">Visiting Team Year/Era:</label>
-                            <input
-                                type="text"
-                                id="visitingTeamYearEra"
-                                name="visitingTeamYearEra"
-                                value={formData.visitingTeamYearEra}
-                                onChange={handleChange}
-                            />
-                        </fieldset>
-                        <button type="submit">Submit</button>
+                            <fieldset className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                                <div className="sm:col-span-3">
+                                    <label htmlFor="visitingTeamName"
+                                           className="block text-sm/6 font-medium text-gray-900">Away Team
+                                        Year/Era:</label>
+                                    <div className="mt-2 grid grid-cols-1">
+                                        <select
+                                            className="block w-full appearance-none rounded-md bg-white py-1.5 pr-8 pl-3 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                                            id="visitingTeamName"
+                                            name="visitingTeamName"
+                                            value={formData.visitingTeamName}
+                                            onChange={handleChange}
+                                            required
+                                        >
+                                            <option>2001</option>
+                                            <option>1991</option>
+                                            <option>1981</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div className="sm:col-span-3">
+                                    <label htmlFor="visitingTeamYearEra"
+                                           className="block text-sm/6 font-medium text-gray-900">Away Team Name:</label>
+                                    <div className="mt-2">
+                                        <select
+                                            className="block w-full appearance-none rounded-md bg-white py-1.5 pr-8 pl-3 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                                            id="visitingTeamYearEra"
+                                            name="visitingTeamYearEra"
+                                            value={formData.visitingTeamYearEra}
+                                            onChange={handleChange}
+                                        >
+                                            <option>Dallas Cowboys</option>
+                                            <option>Miami Dolphins</option>
+                                            <option>New York Giants</option>
+                                        </select>
+                                    </div>
+                                </div>
+                            </fieldset>
+                            <fieldset className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                                <button
+                                    type="submit"
+                                    className="rounded-lg bg-rose-400 w-100 md:w-64 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-rose-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                                >Submit
+                                </button>
+                            </fieldset>
+                        </div>
                     </div>
-                </div>
-            </Form>
+                </Form>
+            </section>
         </Container>
 
     );
--- microleague_fe/components/layout/Container.tsx
+++ microleague_fe/components/layout/Container.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Container = ({children}: {children: React.ReactNode}) => {
     return (
-        <div className="max-w-[1920px] w-full mx-auto px-4 py-4 xl:px-20 ">
+        <div className="max-w-[1920px] w-full mx-auto px-4 py-4 xl:px-20">
             {children}
         </div>
     );
